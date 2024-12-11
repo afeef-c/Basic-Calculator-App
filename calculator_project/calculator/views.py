c1748f02dffd739fe2bc815df2ca4cf3e6336d28
afeef-c <afeefc123@gmail.com>
2024-12-11 07:56:16 +0530

    some correction in view function

--- calculator_project/calculator/views.py
+++ calculator_project/calculator/views.py
@@ -5,41 +5,6 @@ def home(request):
     return render(request, 'calculator/home.html')
 
 
-def calculate(request):
-    if request.method == 'POST':
-        num1 = request.POST.get('num1')
-        num2 = request.POST.get('num2')
-        operation = request.POST.get('operation')
-        print(num1, type(num1))
-        try:
-
-            num1 = float(num1)
-            num2 = float(num2)
-
-            if operation == 'addition':
-                result = num1 + num2
-            elif operation == 'subtraction':
-                result = num1 - num2
-            elif operation == 'multiplication':
-                result = num1 * num2
-            elif operation == 'division':
-                if num2 == 0:
-                    raise ZeroDivisionError("Cannot divide by zero.")
-                result = num1 / num2
-            else:
-                result = None
-
-            return render(request, 'calculator/home.html', {'result': result})
-
-        except ValueError:
-            error = "Invalid input. Please enter numeric values."
-        except ZeroDivisionError as zde:
-            error = str(zde)
-
-        return render(request, 'calculator/home.html', {'error': error})
-
-    return render(request, 'calculator/home.html', {'result': None})
-
 
 def calculate(request):
     if request.method == 'POST':
